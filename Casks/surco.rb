a482356c90715e8b50d3d265e63980166430bb89
Vicent Gozalbes <vigosan@gmail.com>
2026-07-16 12:32:57 +0200

    Point the cask at surco-app after the org transfer

--- Casks/surco.rb
+++ Casks/surco.rb
@@ -5,13 +5,13 @@ cask "surco" do
   sha256 arm:   "0ccb84b07993851c8f7b465bf3663d6349ccd981e4a7f9cb98f9a692f50b1a89",
          intel: "35e265e3a3bddf9ca374fa6dd112d7cbf22c5a3b346668d5a191eb9f1b5dee8f"
 
-  url "https://github.com/vigosan/surco-releases/releases/download/v#{version}/Surco-#{version}-#{arch}.dmg"
+  url "https://github.com/surco-app/surco-releases/releases/download/v#{version}/Surco-#{version}-#{arch}.dmg"
   name "Surco"
   desc "Audio track organizer for DJs"
-  homepage "https://github.com/vigosan/surco-releases"
+  homepage "https://github.com/surco-app/surco-releases"
 
   livecheck do
-    url "https://github.com/vigosan/surco-releases"
+    url "https://github.com/surco-app/surco-releases"
     strategy :github_latest
   end
 
